ef411c57453a75a6b128fdeae794022f4a1bfbc9
svayambhuevents <svayambhuevents@gmail.com>
2026-07-28 09:09:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -25,32 +25,3 @@ document.getElementById("loadBtn").addEventListener("click", async () => {
     console.error(err);
   }
 });
-
-// fetch(url)
-//   .then(response => response.json())
-//   .then(data => {
-//     console.log(data);
-//     document.getElementById("result").textContent =
-//       JSON.stringify(data, null, 2);
-//   })
-//   .catch(error => console.error(error));
-
-// const url = "https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxx/exec";
-
-// const payload = {
-//   name: "John",
-//   age: 25
-// };
-
-// fetch(url, {
-//   method: "POST",
-//   headers: {
-//     "Content-Type": "application/json"
-//   },
-//   body: JSON.stringify(payload)
-// })
-// .then(response => response.json())
-// .then(data => {
-//   console.log(data);
-// })
-.catch(error => console.error(error));
