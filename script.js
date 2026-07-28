875cb64c459526d4f79f40a996d5d69e18b292eb
svayambhuevents <svayambhuevents@gmail.com>
2026-07-28 09:27:45 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,50 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbw7phBMWubjsUUpwIwCqgc4eMOr8iZmTt4yLkqF2416Jo93C4tQoTq2LuFejWJlbba5/exec";
 
-document.getElementById("loadBtn").addEventListener("click", async () => {
-  try {
-    const response = await fetch(API_URL);
+// document.getElementById("loadBtn").addEventListener("click", async () => {
+//   try {
+//     const response = await fetch(API_URL);
 
-    if (!response.ok) {
-      throw new Error(`HTTP ${response.status}`);
-    }
+//     if (!response.ok) {
+//       throw new Error(`HTTP ${response.status}`);
+//     }
 
-    const data = await response.json();
+//     const data = await response.json();
 
-    document.getElementById("result").textContent =
-      JSON.stringify(data, null, 2);
+//     document.getElementById("result").textContent =
+//       JSON.stringify(data, null, 2);
 
-  } catch (err) {
-    console.error(err);
-  }
+//   } catch (err) {
+//     console.error(err);
+//   }
+// });
+
+document.getElementById("getBtn").addEventListener("click", async () => {
+  fetch(API_URL)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      document.getElementById("result").textContent =
+        JSON.stringify(data, null, 2);
+    })
+    .catch(error => console.error(error));
+}
+
+document.getElementById("postBtn").addEventListener("click", async () => {
+  const payload = {
+    name: "John",
+    age: 25
+  };
+  
+  fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(payload)
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => console.error(error));
 });
