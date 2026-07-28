b343b83cbef19beb4eeec572aa2c5ea33e4055cd
svayambhuevents <svayambhuevents@gmail.com>
2026-07-28 09:01:08 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("btn");
+const message = document.getElementById("message");
+
+button.addEventListener("click", () => {
+    message.textContent = "Hello from JavaScript! 🎉";
+});
