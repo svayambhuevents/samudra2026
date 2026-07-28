851f499c00bd136e2b1aade3ca1828dbd7eeb79c
svayambhuevents <svayambhuevents@gmail.com>
2026-07-28 19:07:04 +0800

    Update script.js

--- script.js
+++ script.js
@@ -139,7 +139,7 @@ function submitForm() {
             e_file.innerHTML = "Please select a file.";
             error = true;
         }
-        if (file.size > 1 * 1024 * 1024) {
+        if (file.size > 10 * 1024 * 1024) {
             e_file.innerHTML = "Maximum file size is 10 MB.";
             error = true;
         }
