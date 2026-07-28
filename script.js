f0c030421d7bf14f269a94557fd2923bdacba5a1
svayambhuevents <svayambhuevents@gmail.com>
2026-07-28 09:41:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,5 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbw7phBMWubjsUUpwIwCqgc4eMOr8iZmTt4yLkqF2416Jo93C4tQoTq2LuFejWJlbba5/exec";
 
-// document.getElementById("loadBtn").addEventListener("click", async () => {
-//   try {
-//     const response = await fetch(API_URL);
-
-//     if (!response.ok) {
-//       throw new Error(`HTTP ${response.status}`);
-//     }
-
-//     const data = await response.json();
-
-//     document.getElementById("result").textContent =
-//       JSON.stringify(data, null, 2);
-
-//   } catch (err) {
-//     console.error(err);
-//   }
-// });
-
 document.getElementById("getBtn").addEventListener("click", async () => {
   fetch(API_URL)
     .then(response => response.json())
@@ -30,17 +12,16 @@ document.getElementById("getBtn").addEventListener("click", async () => {
 });
 
 document.getElementById("postBtn").addEventListener("click", async () => {
-  const payload = {
-    name: "John",
-    age: 25
-  };
+  const formData = new FormData();
+  formData.append("name", "John");
+  formData.append("age", "25");
   
   fetch(API_URL, {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
     },
-    body: JSON.stringify(payload)
+    body: formData
   })
   .then(response => response.json())
   .then(data => {
